6a33dcb7c3d6e294afa49d8d09deff306f063e81
thanyawit Thanyawit <thanyawit.nitipornwattana@gmail.com>
2025-03-31 15:55:33 +0700

    create : init for dotenv

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
+	"gin/initializers"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
 	fmt.Println("Start project")
-
 	r := gin.Default()
 
-	// ตั้งค่า routes ผ่าน controller
-	// controller.SetupRoutes(r)
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Message": "pong",
+		})
+	})
 
 	// run at port 8080
 	if err := r.Run(":8080"); err != nil {
